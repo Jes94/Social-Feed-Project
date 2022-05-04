4bb4fc03170bdd1dbdd2e1dad465e20f8f4c3a3b
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-05-04 11:52:11 -0400

    Built NavBar Component

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+const NavBar = () => {
+    return (
+        <a href="/">
+        <h1 style={{margin: '1em'}}>Social
+        <small className='text-muted'>Feed</small></h1>
+        </a>
+    )}
+export default NavBar;
